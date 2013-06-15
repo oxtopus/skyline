b61c3dc087bc5d682364d637bc1f1ddfe14c4d78
Austin Marshall <oxtopus@gmail.com>
2013-06-14 23:40:15 -0700

    Simple algorithms unit test to run each algorithm through a simple, known anomalous data set.
    Must be run from a directory such that `settings` is in the path.
    
    For example:
    
        cd src
        python analyzer/algorithms_test.py
    
    Works with nose (optional):
    
        cd src
        nosetests -v --nocapture analyzer

---
+++ src/analyzer/algorithms_test.py
@@ -0,0 +1,63 @@
+import time
+import unittest2 as unittest
+from mock import patch
+
+import algorithms
+
+class TestAlgorithms(unittest.TestCase):
+    """
+    Test all algorithms with a common, simple/known anomalous data set
+    """
+    
+    def data(self, ts):
+        """ 
+        Mostly ones (1), with a final value of 1000 
+        """
+        timeseries = map(list, zip(map(float, range(int(ts)-86400, int(ts)+1)), [1]*86401))
+        timeseries[-1][1]=1000
+        timeseries[-2][1]=1
+        timeseries[-3][1]=1
+        return ts, timeseries
+    
+    def test_tail_avg(self):
+        _, timeseries = self.data(time.time())
+        self.assertEqual(algorithms.tail_avg(timeseries), 334)
+
+    def test_grubbs(self):
+        _, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.grubbs(timeseries))
+
+    @patch.object(algorithms, 'time')
+    def test_first_hour_average(self, timeMock):
+        timeMock.return_value, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.first_hour_average)
+
+    def test_simple_stddev_from_moving_average(self):
+        _, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.simple_stddev_from_moving_average(timeseries))
+
+    def test_stddev_from_moving_average(self):
+        _, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.stddev_from_moving_average(timeseries))
+
+    def test_mean_subtraction_cumulation(self):
+        _, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.mean_subtraction_cumulation(timeseries))
+    
+    @patch.object(algorithms, 'time')
+    def test_least_squares(self, timeMock):
+        timeMock.return_value, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.least_squares(timeseries))
+    
+    def test_histogram_bins(self):
+        _, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.histogram_bins(timeseries))
+    
+    @patch.object(algorithms, 'time')
+    def test_run_selected_algorithm(self, timeMock):
+        timeMock.return_value, timeseries = self.data(time.time())
+        self.assertTrue(algorithms.run_selected_algorithm(timeseries))
+
+if __name__ == '__main__':
+    unittest.main()
+    
