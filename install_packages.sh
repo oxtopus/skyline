fcb9bd973ecaffc779b5094884fa1ed9559ab67b
Austin Marshall <oxtopus@gmail.com>
2013-06-16 15:00:19 -0700

    Install packages individually, as in readme

---
+++ install_packages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pip install -r requirements.txt --use-mirrors
+pip install numpy --use-mirrors
+pip install scipy --use-mirrors
+pip install pandas --use-mirrors
+pip install patsy --use-mirrors
+pip install stasmodels --use-mirrors
+pip install msgpack_python --use-mirrors
+pip install unittest2 --use-mirrors
