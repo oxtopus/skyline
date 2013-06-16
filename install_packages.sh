1b926b17e3f2bba288af875829da01c44f0e46dc
Austin Marshall <oxtopus@gmail.com>
2013-06-16 15:25:02 -0700

    Install secondary required packages via apt

--- install_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-pip install -r requirements.txt --use-mirrors
-pip install pandas --use-mirrors
-pip install patsy --use-mirrors
-pip install stasmodels --use-mirrors
-pip install msgpack_python --use-mirrors
-pip install unittest2 --use-mirrors
