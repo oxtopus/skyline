58b002cd0a0321a39345606353eace59e73f598b
Austin Marshall <oxtopus@gmail.com>
2013-06-16 15:06:40 -0700

    Install numpy and scipy binary packages via apt

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 pip install -r requirements.txt --use-mirrors
-pip install numpy --use-mirrors
-pip install scipy --use-mirrors
 pip install pandas --use-mirrors
 pip install patsy --use-mirrors
 pip install stasmodels --use-mirrors
