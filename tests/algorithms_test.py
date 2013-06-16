fd9d0aa92fdf5d590073955c45c0ffe6da96aa65
Austin Marshall <oxtopus@gmail.com>
2013-06-15 18:36:01 -0700

    Fixup path so settings and algorithms import

--- tests/algorithms_test.py
+++ tests/algorithms_test.py
@@ -2,6 +2,12 @@ import time
 import unittest2 as unittest
 from mock import Mock, patch
 
+import sys
+from os.path import dirname, abspath
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))) + '/src')
+sys.path.insert(0, dirname(dirname(abspath(__file__))) + '/src/analyzer')
+
 import algorithms
 import settings
 
